8f6e3618e5e6d6f24d6a60e6f77624736c1b095b
123vivekr <123vivekr@gmail.com>
2018-08-18 13:41:59 +0530

    store data to csv

--- script.py
+++ script.py
@@ -1,22 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import csv
 
-# set chrome headless
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-browser = webdriver.Chrome(chrome_options=chrome_options)
+class Points():
 
-# scrape sites
-url = "https://freecodecamp.org/"
-usernames = ["123vivekr", "isht3", "ak04"]
+    def __init__(self):
+        self.url = "https://freecodecamp.org/"
+        self.usernames = []
+        fieldnames = ["user_name", "initial_points", "current_points"]
+        csvfile = open('usernames.csv', 'w', newline='')
+        self.writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        self.writer.writeheader()
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        self.browser = webdriver.Chrome(chrome_options=chrome_options)
 
-# points holder
-points = []
-
-for username in usernames:
-    browser.get(url + username)
-    browser.implicitly_wait(30)
-    point = browser.find_element_by_class_name('points').text
-    points.append({username: point[:-7]})
-
-print(points)
+    def add_username(self, username):
+        initial_point = self.get_points(username)
+        self.writer.writerow({'user_name': username, "initial_points": initial_point})
+    
+    def get_points(self, username):
+        self.browser.get(self.url + username)
+        self.browser.implicitly_wait(30)
+        point = self.browser.find_element_by_class_name('points').text
+        return point
